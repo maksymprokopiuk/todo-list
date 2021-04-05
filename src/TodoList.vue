356b842aba13b8815e69d0d18e27ba6c0d7fdcc6
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2021-04-05 13:40:18 +0300

    created TodoList & TodoItem components

---
+++ src/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <todo-item />
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  import TodoItem from '@/components/TodoItem'
+
+  export default {
+    name: 'TodoList',
+
+    components: {
+      TodoItem,
+    },
+
+    computed: {
+      ...mapGetters(['getTodoList'])
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
