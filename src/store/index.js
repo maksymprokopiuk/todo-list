909dc4227a91a5cf7d0402474419d7b2bdcbe56c
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2021-04-06 17:29:06 +0300

    added state

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,104 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
+
   state: {
+    todos: [],
+    search: '',
+    sortByDate: false,
+    completed: false,
   },
   mutations: {
+    addTodo(state, todo) {
+      state.todos.push(todo)
+    },
+
+    deleteTodo(state, id) {
+      state.todos = state.todos.filter(
+        todo => todo.id !== id
+      )
+    },
+
+    updateTodo(state, todo) {
+      const currentTodo = state.todos.findIndex(
+        item => item.id === todo.id
+      )
+      state.todos[currentTodo] = todo
+    },
+
+    completedTodo(state, id) {
+      const currentTodo = state.todos.findIndex(
+        item => item.id === id
+      )
+      state.todos[currentTodo].completed = !state.todos[currentTodo].completed
+    },
+
+    setSearchWord(state, word) {
+      state.search = word
+    },
+
+    setSortDate(state, sort) {
+      state.sortByDate = sort
+    },
+
+    setCompleted(state, completed) {
+      state.completed = completed
+    },
   },
   actions: {
+    addTodo({commit}, todo) {
+      commit('addTodo', todo)
+    },
+
+    deleteTodo({commit}, id) {
+      commit('deleteTodo', id)
+    },
+    
+    updateTodo({commit}, todo) {
+      commit('updateTodo', todo)
+    },
+    
+    completedTodo({commit}, id) {
+      commit('completedTodo', id)
+    },
+
+    setSearchWord({commit}, word) {
+      commit('setSearchWord', word)
+    },
+
+    setSortDate({commit}, sort) {
+      commit('setSortDate', sort)
+    },
+
+    setCompleted({commit}, completed) {
+      commit('setCompleted', completed)
+    },
   },
   getters: {
+    getTodos: state => {
+      // const a = state.todos.filter(todo => todo.title.toLowerCase().includes(state.search.toLowerCase()))
+      // if (state.sortByDate) {
+      //   return a.sort((a, b) => a.date < b.date ? 1 : -1)
+      // } else {
+      //   return a.sort((a, b) => a.date > b.date ? 1 : -1)
+      // }
+      const a = state.todos.filter(todo => 
+        todo.title.toLowerCase().includes(state.search.toLowerCase()))
+        
+      const b = state.sortByDate ? a.sort((a, b) => a.date < b.date ? 1 : -1) : a.sort((a, b) => a.date > b.date ? 1 : -1)
+
+      const c = state.completed ? b.filter(todo => todo.completed === true) : b.filter(todo => todo.completed === false)
+      return c
+      
+    },
+
+    getSearchWord: state => state.search,
+    getSortByDate: state => state.sortByDate,
   },
   modules: {
   }
